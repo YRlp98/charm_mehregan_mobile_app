e9fa01d9c594ceb5b4680fa166402f3f853e1a41
YRlp98 <yousef.r1999@gmail.com>
2019-09-18 19:28:19 +0430

    add routes

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Charm Mehregan',
-        debugShowCheckedModeBanner: false,
-        theme: appTheme,
-        home: new Directionality(
-            textDirection: TextDirection.rtl, child: SplashScreen()));
+      title: 'Charm Mehregan',
+      debugShowCheckedModeBanner: false,
+      theme: appTheme,
+      home: new Directionality(
+          textDirection: TextDirection.rtl, child: SplashScreen()),
+
+//      Routes
+//      routes: {'/': (context) => new SplashScreen()},
+    );
   }
 }
