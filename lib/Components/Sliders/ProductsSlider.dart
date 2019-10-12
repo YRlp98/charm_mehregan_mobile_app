d59a3b5a630d51791d1ed29869bddddf3a939f76
YRlp98 <yousef.r1999@gmail.com>
2019-10-12 19:29:58 +0330

    Make slider responsize

--- lib/Components/Sliders/ProductsSlider.dart
+++ lib/Components/Sliders/ProductsSlider.dart
@@ -23,11 +23,11 @@ class ProductsSliderCreate extends StatelessWidget {
         child: new ClipRRect(
             borderRadius: new BorderRadius.circular(10),
             child: new Carousel(
-              boxFit: BoxFit.fill,
+              boxFit: BoxFit.cover,
               animationCurve: Curves.fastOutSlowIn,
               animationDuration: Duration(milliseconds: 3000),
               autoplayDuration: Duration(seconds: 5),
-              dotSize: 5,
+              dotSize: 0.64 * SizeConfig.heightMultiplier,
               dotSpacing: 10,
               dotBgColor: Colors.transparent,
               dotPosition: DotPosition.bottomRight,
@@ -35,7 +35,7 @@ class ProductsSliderCreate extends StatelessWidget {
               images: [
                 new GestureDetector(
                   child: new CachedNetworkImage(
-                    fit: BoxFit.fill,
+                    fit: BoxFit.cover,
                     imageUrl:
                         'https://www.paperhigh.com/media/catalog/product/cache/1/thumbnail/600x600/9df78eab33525d08d6e5fb8d27136e95/l/o/low_res_large_vintage_leather_stachel_llptl_lifestyle.jpg',
                     placeholder: (context, url) =>
@@ -48,7 +48,7 @@ class ProductsSliderCreate extends StatelessWidget {
                 ),
                 new GestureDetector(
                   child: new CachedNetworkImage(
-                    fit: BoxFit.fill,
+                    fit: BoxFit.cover,
                     imageUrl:
                         'https://ruitertassen.com/wp-content/uploads/2018/04/brown-leather-laptop-backpack-satchel-for-men-732837.jpg',
                     placeholder: (context, url) =>
@@ -61,7 +61,7 @@ class ProductsSliderCreate extends StatelessWidget {
                 ),
                 new GestureDetector(
                   child: new CachedNetworkImage(
-                    fit: BoxFit.fill,
+                    fit: BoxFit.cover,
                     imageUrl:
                         'https://ruitertassen.com/wp-content/uploads/2018/04/durable-vegetable-tanned-brown-leather-satchel-bag-732337.jpg',
                     placeholder: (context, url) =>
@@ -74,7 +74,7 @@ class ProductsSliderCreate extends StatelessWidget {
                 ),
                 new GestureDetector(
                   child: new CachedNetworkImage(
-                    fit: BoxFit.fill,
+                    fit: BoxFit.cover,
                     imageUrl:
                         'https://ruitertassen.com/wp-content/uploads/2018/04/large-mens-leather-backpack-satchel-732237.jpg',
                     placeholder: (context, url) =>
