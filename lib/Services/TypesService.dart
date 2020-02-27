a6099df58038a9a292d15d41c5fa360308d396ff
Yousef Roshandel <Yousef.r1999@gmail.com>
2020-02-27 18:28:03 +0330

    Create Type s servies

--- lib/Services/TypesService.dart
+++ lib/Services/TypesService.dart
@@ -3,30 +3,17 @@ import 'package:charm_mehregan/Models/TypesModel.dart';
 import 'package:http/http.dart' as http;
 
 class TypesService {
-  static Future<Map> getTypes(int page) async {
-    final types = await http.get('https://charm.liara.run/api/v1/products');
-
-    // const URL = 'https://charm.liara.run';
-    // const VERSION = 'https://charm.liara.run';
-
-    // final types = await http.get('${URL}/api/${VERSION}/types?${page}');
-    // show types to users
-
-    // If user chose the type
-    // فیلتر کردن با تایپ
-    // page=&type=${type['id']}
-
-    // Else    final products = await http.get('${URL}/api/${VERSTION}/products?page=${page}');
-
-    if (types.statusCode == 200) {
-      var typesBody = json.decode(types.body)['data'][0];
-      List<TypesModel> typesList = [];
-      typesBody['data'][0].forEach((item) {
-        typesList.add(TypesModel.fromJson(item));
-      });
-
-      return {'typeList': typesList};
-      // return {'current_page': typesBody['current_page'], 'typeList': typesList};
+  static Future<TypesModel> getProducts() async {
+    print('**** getTypes ****');
+    String typesUrl = "https://charm.liara.run//api/v1/products";
+    TypesModel typeList;
+    var response = await http.get(typesUrl);
+    if (response.statusCode == 200) {
+      typeList = TypesModel.fromJson(jsonDecode(response.body));
+    } else {
+      print('Faild to load data from API');
     }
+
+    return typeList;
   }
 }
